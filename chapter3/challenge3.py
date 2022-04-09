7bfdb43e3e85998a2bb0c4a8780ee588ecf322bb
坂井佑 <yusakai@sakaiyuunoMacBook-Air.local>
2022-04-09 19:13:20 +0900

    chapter3

---
+++ chapter3/challenge3.py
@@ -0,0 +1,8 @@
+x = 1
+
+if x <= 10:
+    print("１０以下")
+elif x > 10 and x <= 25:
+    print("10より大きく25以下")
+elif x > 25:
+    print("25より大きい")
---
+++ chapter3/challenge6.py
@@ -0,0 +1,6 @@
+age = 18
+
+if age < 18:
+    print("18未満")
+else:
+    print("18以上")
